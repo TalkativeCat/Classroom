7d45812f4f881c483dc8c45b718a27907778618b
Денис <karasov-dv@ural.rt.ru>
2023-10-28 14:31:59 +0500

    Отладка DataGroup + интеграция в методы лямбд

--- src/main/java/DataGroup.java
+++ src/main/java/DataGroup.java
@@ -5,24 +5,7 @@ import java.util.function.Function;
 class DataGroup<T> {
     Reader reader = new Reader();
 
-    public DataGroup(ByFamily byFamily) {
-    }
-    public DataGroup(ByAge byAge) {
-    }
-
-    public DataGroup() {
-
-    }
-
-    @FunctionalInterface
-    interface ByFamily {
-        String family(Person person);
-    }
-    @FunctionalInterface
-    interface ByAge {
-        int age(Person person);
-    }
-    public HashMap<T, ArrayList<Person>> addPerson(Function<Person, T> classifier) {
+    public Person[] addPerson(Function<Person, T> classifier, T variable) {
 
         HashMap<T, ArrayList<Person>> sortedMap = new HashMap<>();
 
@@ -30,7 +13,6 @@ class DataGroup<T> {
             T key = classifier.apply(persons);
             sortedMap.computeIfAbsent(key, k -> new ArrayList<>()).add(persons);
         }
-        return sortedMap;
+        return sortedMap.get(variable).toArray(new Person[0]);
     }
-
 }
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,24 +1,19 @@
 import java.util.Scanner;
 
-
 public class MainClass {
-    static ClassroomDataGroups classroomDataGroups = new ClassroomDataGroups();
-    static PersonAgeDataGroups personAgeDataGroups = new PersonAgeDataGroups();
-    static PersonNameDataGroup personNameDataGroup = new PersonNameDataGroup();
 
     public static void main(String[] args) {
         getPersonsByGroup(10, 11);
         getPersonsByAge(14);
         getPersonsByFamilyBinary();
-//        getPersonsByFamily();
-
     }
 
     // Собираем в массивы данные учеников 10 и 11 классов, после чего склеиваем их в единый массив.
     // Поскольку для вычисления средней оценки нужно будет перебрать все записи, подойдет обычный цикл
     public static void getPersonsByGroup(int group1, int group2) {
-        Person[] persons1 = classroomDataGroups.getPersons(group1);
-        Person[] persons2 = classroomDataGroups.getPersons(group2);
+        DataGroup<Integer> dataGroupGroup = new DataGroup<>();
+        Person[] persons1 = dataGroupGroup.addPerson(Person::getGroup, group1);
+        Person[] persons2 =dataGroupGroup.addPerson(Person::getGroup, group2);
         Person[] persons = new Person[persons1.length + persons2.length];
         System.arraycopy(persons1, 0, persons, 0, persons1.length);
         System.arraycopy(persons2, 0, persons, persons1.length, persons2.length);
@@ -31,49 +26,38 @@ public class MainClass {
         System.out.println("Средний балл в " + group1 + " и " + group2 + " классах: " + result + "\n");
     }
 
-//    public static void getPersonsByFamily() {
-//        System.out.println("Введите фамилию студента: ");
-//        Scanner console = new Scanner(System.in);
-//        String family = console.nextLine();
-//        for (Person obj : personNameDataGroup.getPersons(family)) {
-//            if (obj.getFamily().equals(family)) {
-//                System.out.println(obj.getFamily() + " " + obj.getName());
-//            }
-//        }
-//    }
-
     // В методе используется отсортированный массив, полученный по индексу первой буквы имени студента из HashMap.
     // По нему быстрее будет выполняться бинарный поиск
     public static void getPersonsByFamilyBinary() {
+        DataGroup<String> dataGroupName = new DataGroup<>();
         System.out.println("Введите фамилию студента: ");
         Scanner console = new Scanner(System.in);
         String family = console.nextLine();
         int left = 0;
-        int right = personNameDataGroup.getPersons(family).length - 1;
+        int right = dataGroupName.addPerson(Person::getFamily, family).length - 1;
         boolean found = false;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            int compareResult = personNameDataGroup.getPersons(family)[mid].getFamily().compareTo(family);
+            int compareResult = dataGroupName.addPerson(Person::getFamily, family)[mid].getFamily().compareTo(family);
 
             if (compareResult == 0) {
                 // Строка family найдена, выводим ее в консоль
-                System.out.println(personNameDataGroup.getPersons(family)[mid].getFamily() + " " + personNameDataGroup.getPersons(family)[mid].getName());
+                System.out.println(dataGroupName.addPerson(Person::getFamily, family)[mid].getFamily() + " " + dataGroupName.addPerson(Person::getFamily, family)[mid].getName());
                 found = true;
 
                 // Проверяем наличие дополнительных совпадающих строк слева
                 int index = mid - 1;
-                while (index >= 0 && personNameDataGroup.getPersons(family)[index].getFamily().equals(family)) {
-                    System.out.println(personNameDataGroup.getPersons(family)[index].getFamily() + " " + personNameDataGroup.getPersons(family)[index].getName());
+                while (index >= 0 && dataGroupName.addPerson(Person::getFamily, family)[index].getFamily().equals(family)) {
+                    System.out.println(dataGroupName.addPerson(Person::getFamily, family)[index].getFamily() + " " + dataGroupName.addPerson(Person::getFamily, family)[index].getName());
                     index--;
                 }
 
                 // Проверяем наличие дополнительных совпадающих строк справа
                 index = mid + 1;
-                while (index < personNameDataGroup.getPersons(family).length && personNameDataGroup.getPersons(family)[index].getFamily().equals(family)) {
-                    System.out.println(personNameDataGroup.getPersons(family)[index].getFamily() + " " + personNameDataGroup.getPersons(family)[index].getName());
+                while (index < dataGroupName.addPerson(Person::getFamily, family).length && dataGroupName.addPerson(Person::getFamily, family)[index].getFamily().equals(family)) {
+                    System.out.println(dataGroupName.addPerson(Person::getFamily, family)[index].getFamily() + " " + dataGroupName.addPerson(Person::getFamily, family)[index].getName());
                     index++;
                 }
-
                 break;
             } else if (compareResult < 0) {
                 // Строка family находится в правой половине
@@ -83,7 +67,6 @@ public class MainClass {
                 right = mid - 1;
             }
         }
-
         if (!found) {
             System.out.println("Такая фамилия не найдена");
         }
@@ -92,10 +75,11 @@ public class MainClass {
     //Поочередно получаем массивы данных студенов из HashMap по ключу - возрасту (начиная с 15 и до 17 лет).
     // Суммируем баллы по всем предметам у каждого ученика, при совпадении максимально возможной суммы выводим данные в консоль
     public static void getPersonsByAge(int age) {
+        DataGroup<Integer> dataGroupAge = new DataGroup<>();
         System.out.println("Отличники среди студентов старше 14 лет:");
         age += 1;
         while ((age < 18)) {
-            for (Person obj : personAgeDataGroups.getPersons(age)) {
+            for (Person obj : dataGroupAge.addPerson(Person::getAge, age)) {
                 int number = obj.getPhysics() + obj.getMathematics() + obj.getRus() + obj.getLiterature() + obj.getGeometry() + obj.getInformatics();
                 if (number == 30) {
                     System.out.println(obj.getFamily() + " " + obj.getName());
@@ -105,6 +89,4 @@ public class MainClass {
         }
         System.out.println();
     }
-
-
 }
