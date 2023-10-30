f23f6ae1de8eeb7f9a82356751afd7570a268936
Денис <karasov-dv@ural.rt.ru>
2023-10-30 15:48:52 +0500

    Добавил абстрактные типы при инициализации HashMap, устранил перевызывание AddPerson

--- src/main/java/PersonAgeDataGroups.java
+++ src/main/java/PersonAgeDataGroups.java
@@ -1,22 +1,22 @@
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
 public class PersonAgeDataGroups {
     Reader reader = new Reader();
-    public HashMap<Integer, ArrayList<Person>> addPerson() {
 
-        HashMap<Integer, ArrayList<Person>> sortedMap = new HashMap<>();
+    private final HashMap<Integer, List<Person>> sortedMap = new HashMap<>();
+    public void addPerson() {
 
         for (Person persons : reader.readCsv()) {
             Integer age = persons.getAge();
             sortedMap.computeIfAbsent(age, k -> new ArrayList<>()).add(persons);
         }
-        return sortedMap;
     }
     public Person[] getPersons(int age) {
-
-        return addPerson().get(age).toArray(new Person[0]);
+        if (sortedMap.isEmpty()) {
+            addPerson();
+        }
+        return sortedMap.get(age).toArray(new Person[0]);
     }
 }
