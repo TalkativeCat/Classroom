15a48d85a417cb3f60f6b1278d26354732f8f2e0
Денис <karasov-dv@ural.rt.ru>
2023-11-01 01:14:22 +0500

    Добавление init.sql

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,4 +1,5 @@
 import dataGroup.CommandBuilder;
+
 import java.util.Arrays;
 
 public class MainClass {
@@ -8,11 +9,14 @@ public class MainClass {
         // которые копируются со смещением в этот же массив и передаются для дальнейших вычислений в метод execute()
 
         // пример аргументов при запуске программы: getGroup 10 11
-
-        String command = args[0];
-        args = Arrays.copyOfRange(args, 1, args.length);
-        CommandBuilder commandBuilder = new CommandBuilder();
-        commandBuilder.selectCommand(command, args);
+        if (args.length == 0) {
+            System.out.println("Вы не ввели ни одной команды");
+        } else {
+            String command = args[0];
+            args = Arrays.copyOfRange(args, 1, args.length);
+            CommandBuilder commandBuilder = new CommandBuilder();
+            commandBuilder.selectCommand(command, args);
+        }
     }
 
 }
