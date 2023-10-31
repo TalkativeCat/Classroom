9be536b194c3c2f32e5ac8a48e01a452678d183f
Денис <karasov-dv@ural.rt.ru>
2023-11-01 01:14:22 +0500

    Дополнение

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -6,6 +6,9 @@ public class MainClass {
     public static void main(String[] args) {
         // из массива аргументов вытаскивется под индексом [0] - это команда, распознающаяся в CommandBuilder, все элементы под индексами далее - аргументы,
         // которые копируются со смещением в этот же массив и передаются для дальнейших вычислений в метод execute()
+
+        // пример аргументов при запуске программы: getGroup 10 11
+
         String command = args[0];
         args = Arrays.copyOfRange(args, 1, args.length);
         CommandBuilder commandBuilder = new CommandBuilder();
