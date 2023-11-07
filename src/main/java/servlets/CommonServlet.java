f085be7aecda898dd632d94ae4d741fdd6b52502
Денис <karasov-dv@ural.rt.ru>
2023-11-07 16:24:24 +0500

    Добавление зависимостей и сервлета

---
+++ src/main/java/servlets/CommonServlet.java
@@ -0,0 +1,22 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/*"})
+public class CommonServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (var output = resp.getWriter()){
+            output.write("Превед медвед!");
+            output.flush();
+        }
+    }
+
+}
